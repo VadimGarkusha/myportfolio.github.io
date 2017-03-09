5a62608c35de106b9a53d2a60b6a8026701f6d2b
Vadym Harkusha <vharkush@my.centennialcollege.ca>
2017-03-08 22:01:34 -0400

    Skills | Menu | My projects
    Added animation to skills and changed diagrams for other technical
    skills and personal skills
    Changed top strip color for menu
    Added interactive hover styles for myproject images and modal window

--- JavaScript/scripts.js
+++ JavaScript/scripts.js
@@ -15,7 +15,7 @@ function NonActiveName(i) {
     } else {
         document.getElementsByClassName("label-form")[i].style.color = "#3d3e42";
     }
-    input.setAttribute("style", "width: 100%; border: none!important; border-bottom: 2px solid #3d3e42!important; border-radius: 0px!important; padding-left: 10px; padding-bottom: 5px; margin-top: 5px;");
+    input.setAttribute("style", "width: 100%; border: none!important; border-bottom: 2px solid white!important; border-radius: 0px!important; padding-left: 10px; padding-bottom: 5px; margin-top: 5px;");
 }
 
 
@@ -111,189 +111,37 @@ class StickyNavigation {
 
 new StickyNavigation();
 
-//HEADER
+//SKILLS Animation
 
-var Utils = {
-    Android: function() {
-        return navigator.userAgent.match(/Android/i);
-    },
-    BlackBerry: function() {
-        return navigator.userAgent.match(/BlackBerry/i);
-    },
-    iOS: function() {
-        return navigator.userAgent.match(/iPhone|iPad|iPod/i);
-    },
-    Opera: function() {
-        return navigator.userAgent.match(/Opera Mini/i);
-    },
-    Windows: function() {
-        return navigator.userAgent.match(/IEMobile/i);
-    },
-    any: function() {
-        return (Utils.Android() || Utils.BlackBerry() || Utils.iOS() || Utils.Opera() || Utils.Windows());
-    },
-    randomInRange: function(min, max) {
-        return Math.floor(Math.random() * (max - min + 1)) + min;
+// Returns true if the specified element has been scrolled into the viewport.
+function isElementInViewport(elem) {
+    var $elem = $(elem);
+
+    // Get the scroll position of the page.
+    var scrollElem = ((navigator.userAgent.toLowerCase().indexOf('webkit') != -1) ? 'body' : 'html');
+    var viewportTop = $(scrollElem).scrollTop();
+    var viewportBottom = viewportTop + $(window).height();
+
+    // Get the position of the element on the page.
+    var elemTop = Math.round($elem.offset().top);
+    var elemBottom = elemTop + $elem.height();
+
+    return ((elemTop < viewportBottom) && (elemBottom > viewportTop));
+}
+
+// Check if it's time to start the animation.
+function checkAnimation() {
+    var $elem = $('.skill-list .skill-percentage');
+
+    if (isElementInViewport($elem)) {
+        // Start the animation
+        $elem.addClass('level');
+    } else {
+        $elem.removeClass('level');
     }
-};
+}
 
-var $borders = [].slice.call(document.querySelectorAll('.border-inner'), 0),
-    wrapperRombo = document.getElementById('wrapper'),
-    control = document.getElementById('control'),
-    colors = ['#df3891', '#fff78b', '#692286', '#c4a66b', '#ed95c0', '#6ac1b8'],
-    nRombo = 46,
-    timer = 0.8;
-
-var setObj = function setObj() {
-
-    var zoomLevel = document.documentElement.clientWidth / window.innerWidth;
-    var heightIOs = window.innerHeight * zoomLevel;
-
-    if (Utils.iOS()) {
-
-        if (heightIOs > window.innerWidth) {
-            document.querySelector('.Main').style.height = heightIOs + 'px';
-            document.querySelector('.Main').style.minHeight = heightIOs + 'px';
-        }
-
-    }
-
-    TweenMax.set(document.querySelectorAll('.border-inner')[0], {
-        y: -32
-    });
-    TweenMax.set(document.querySelectorAll('.border-inner')[1], {
-        y: 32
-    });
-    TweenMax.set(document.querySelectorAll('.border-inner')[2], {
-        x: -32
-    });
-    TweenMax.set(document.querySelectorAll('.border-inner')[3], {
-        x: 32
-    });
-
-};
-
-var border = function border() {
-    var tl = new TimelineMax();
-
-    tl.to($borders, 1.8, {
-        x: 0,
-        y: 0,
-        force3D: true,
-        ease: Power1.easeOut,
-        onComplete: function() {
-            document.body.classList.remove('overflow');
-        }
-    });
-
-    return tl;
-};
-
-var romboInit = function romboInit() {
-
-    for (var i = 0; i < nRombo; i++) {
-
-        var gridItem = document.createElement('div');
-        var romboDiv = document.createElement('div');
-
-        wrapperRombo.appendChild(gridItem);
-        gridItem.className = "box";
-
-        TweenMax.set(".box", {
-            perspective: 600,
-            transformOrigin: '50% 50%'
-        });
-
-        document.querySelectorAll('.box')[i].appendChild(romboDiv);
-        romboDiv.className = "rombo";
-
-        TweenMax.set(".rombo", {
-            transformStyle: "preserve-3d"
-        });
-
-        if (Utils.any()) {
-
-            TweenMax.set(document.querySelectorAll('.rombo')[i], {
-                backgroundColor: colors[Math.floor(Math.random() * colors.length)],
-                top: Utils.randomInRange(-40, 40),
-                left: Utils.randomInRange(-40, 40),
-                y: 0,
-                scale: 0,
-                opacity: 0,
-                transformOrigin: '50% 50%',
-                rotationY: Utils.randomInRange(-720, 720),
-                rotation: Utils.randomInRange(-320, 320)
-            });
-
-        } else {
-
-            TweenMax.set(document.querySelectorAll('.rombo')[i], {
-                backgroundColor: colors[Math.floor(Math.random() * colors.length)],
-                top: Utils.randomInRange(-180, 180),
-                left: Utils.randomInRange(-180, 180),
-                y: -100,
-                scale: 0,
-                opacity: 0,
-                transformOrigin: '50% 50%',
-                rotationY: Utils.randomInRange(-720, 720),
-                rotation: Utils.randomInRange(-320, 320)
-            });
-
-        }
-
-    }
-
-};
-
-var romboAnimation = function romboAnimation() {
-    var romboTodo = [].slice.call(document.querySelectorAll('.rombo'), 0);
-    var tl = new TimelineMax();
-
-    tl.staggerTo(romboTodo, 1.2, {
-        y: 0,
-        scale: 1,
-        opacity: 1,
-        rotationY: 0,
-        rotation: '+=240',
-        force3D: true,
-        ease: Power2.easeOut
-    }, 0.08);
-
-    return tl;
-};
-
-var init = function init() {
-
-    setObj();
-    romboInit();
-
-    // MASTER SCENES
-
-    var master = new TimelineMax({
-        delay: 0.4
-    });
-
-    master.add(border(), "scene1")
-        .add(romboAnimation(), "-=1.8", "scene2");
-    master.timeScale(timer);
-
-    function go(el) {
-        master.play();
-        master.timeScale(timer);
-        el.textContent = "REVERSE";
-    }
-
-    function rewards(el) {
-        master.reverse();
-        master.timeScale(timer * 5);
-        el.textContent = "PLAY";
-    }
-
-    control.onclick = function() {
-        master.reversed() ? go(this) : rewards(this);
-        return false;
-    };
-
-};
-
-window.onload = init;
+// Capture scroll events
+$(window).scroll(function() {
+    checkAnimation();
+});;
